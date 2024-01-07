3680b8d47f6860e55d09d1807e3ac68c5fd94120
jeremy.whelchel <jeremy@whelchel.me>
2024-01-07 17:08:03 -0500

    update buttons.py

--- buttons.py
+++ buttons.py
@@ -1,37 +1,52 @@
 #!/usr/bin/python3
 
+import logging
 import RPi.GPIO as GPIO
 import time
 import os
 
-os.system("raspi-gpio set 19 ip")
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(18, GPIO.OUT)
-
 
 def turnOnScreen():
+    logging.info("turnOnScreen")
+    # Enable audio
     os.system("raspi-gpio set 19 op a5")
+    # Turn on screen backlight
     GPIO.output(18, GPIO.HIGH)
 
 
 def turnOffScreen():
+    logging.info("turnOffScreen")
+    # Mute audio
     os.system("raspi-gpio set 19 ip")
+    # Turn off screen backlight
     GPIO.output(18, GPIO.LOW)
 
 
-turnOffScreen()
-screen_on = False
+def main():
+    logging.getLogger().setLevel(logging.INFO)
 
-while True:
-    # If you are having and issue with the button doing the opposite of what you want
-    # IE Turns on when it should be off, change this line to:
-    # input = not GPIO.input(26)
-    inp = GPIO.input(26)
-    if inp != screen_on:
-        screen_on = inp
-        if screen_on:
-            turnOnScreen()
-        else:
-            turnOffScreen()
-    time.sleep(0.3)
+    # Initial GPIO setup
+    GPIO.setmode(GPIO.BCM)
+    # Set pin 26 as input to monitor button press
+    GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    # Set pin 18 as output to control screen backlight
+    GPIO.setup(18, GPIO.OUT)
+
+    turnOffScreen()
+    screen_on = False
+
+    while True:
+        # If you are having and issue with the button doing the opposite of what you want
+        # IE Turns on when it should be off, change this line to:
+        # input = not GPIO.input(26)
+        inp = GPIO.input(26)
+        if inp != screen_on:
+            screen_on = inp
+            if screen_on:
+                turnOnScreen()
+            else:
+                turnOffScreen()
+        time.sleep(0.3)
+
+
+main()
