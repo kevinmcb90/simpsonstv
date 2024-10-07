20b93b579771503cd9f907ad950850c36e0a9ad5
kevinmcb90 <kevinmcbride90@gmail.com>
2024-10-07 22:29:37 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find /videos -name "*.mp4" -print0 | shuf -z | while IFS= read -r -d '' video; do
+find /home/pi/simpsonstv/videos -name "*.mp4" -print0 | shuf -z | while IFS= read -r -d '' video; do
     filename=$(basename "$video")
     filename_no_ext="${filename%.*}"
 
